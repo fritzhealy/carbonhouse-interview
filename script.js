0bf8e548d8f68c585e2d149abd2ab68567409143
fritzhealy <fre.healy@gmail.com>
2018-04-29 17:58:23 -0400

    styling header for mobile

--- script.js
+++ script.js
@@ -2,6 +2,28 @@
     $(document).ready(function(){
         $('.event').matchHeight();
         $('.blocks').matchHeight();
+
+        var $mobile = $('#mobile-menu');
+        $('#hamburger').click(function(){
+            if($mobile.hasClass('active')){
+                $mobile.animate({
+                    left: '-100%'
+                }, 200, function(){
+                    $mobile.removeClass('active');
+                });
+            } else {
+                $mobile.addClass('active').animate({
+                    left: 0
+                },200);
+            }
+        });
+        $('#close-menu').click(function(){
+            $mobile.animate({
+                left: '-100%'
+            }, 200, function(){
+                $mobile.removeClass('active');
+            });
+        })
     });
 })(jQuery);
 /*
