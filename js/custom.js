81a82aabc7b7a2a184e7184a2304cd652e257e8b
fritzhealy <fre.healy@gmail.com>
2018-04-29 22:16:07 -0400

    adding rollovers and tabs

--- js/custom.js
+++ js/custom.js
@@ -23,6 +23,36 @@
             }, 200, function(){
                 $mobile.removeClass('active');
             });
-        })
+        });
+        
+        var $list = $('#list');
+        var $grid = $('#grid');
+        var $events_wrapper = $('#events-wrapper');
+        $list.click(function(){
+            $events_wrapper.removeClass('grid');
+            $events_wrapper.addClass('list');
+            $list.addClass('active');
+            $grid.removeClass('active');
+            $('.event').css('height','');
+        });
+        $grid.click(function(){
+            $events_wrapper.removeClass('list');
+            $events_wrapper.addClass('grid');
+            $grid.addClass('active');
+            $list.removeClass('active');
+            $('.event').matchHeight();
+        });
+
+        var $views = $('.plugin.social .view');
+        var $tabs = $('.plugin.social .social.bar li');
+        $tabs.click(function(){
+            var $this = $(this);
+            $tabs.removeClass('active');
+            $views.removeClass('active');
+            $this.addClass('active');
+            $views.filter(function(i, el){
+                return $this.data('source') === $(el).data('source');
+            }).addClass('active');
+        });
     });
 })(jQuery);
